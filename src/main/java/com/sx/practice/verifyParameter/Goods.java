33a9fd46e8c48ee4c3ab6d5400c06c13176fbd71
shuxin <11783358305@qq.com>
2019-08-12 16:50:13 +0800

    javaBase项目创建

---
+++ src/main/java/com/sx/practice/verifyParameter/Goods.java
@@ -0,0 +1,13 @@
+package com.sx.practice.verifyParameter;
+
+/**商品类
+ * @author shuxin
+ * @date 2019/8/12 - 15:43
+ */
+public class Goods {
+
+    //
+    public String getGoodsById(Integer goodsId){
+        return null;
+    }
+}
